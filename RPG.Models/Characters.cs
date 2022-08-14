cb87e225251d86eb4b4048bddb26590afc99590f
oluwafemi13 <sundayjabikem@gmail.com>
2022-08-14 16:26:23 +0100

    character Model

---
+++ RPG.Models/Characters.cs
@@ -0,0 +1,15 @@
+﻿namespace RPG_Character_API.RPG.Models
+{
+    public class Characters
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int HitPoints { get; set; } = 100;
+        // public int TotalScore { get; set; }
+        public int Strength { get; set; }=10;
+        public int Defence { get; set; } = 10;
+        public int Intelligence { get; set; } = 10;
+        public int Life { get; set; } = 100;
+
+    }
+}
